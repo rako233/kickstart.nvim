504cf76b375342c68626f033710850ab93030ece
rako233 <rako233@users.noreply.github.com>
2024-12-16 21:26:11 +0100

    added plugin for markdown preview

--- lua/custom/plugins/markdown-preview.lua
+++ lua/custom/plugins/markdown-preview.lua
@@ -1,14 +1,16 @@
 return {
   {
     'iamcco/markdown-preview.nvim',
-    enabled = false,
+    enabled = true,
     cmd = { 'MarkdownPreviewToggle', 'MarkdownPreview', 'MarkdownPreviewStop' },
     build = 'cd app && yarn install',
-    init = function()
+    config = function()
       vim.g.mkdp_filetypes = { 'markdown' }
+      vim.g.mkdp_auto_start = 0
+      vim.g.mkdp_refresh_slow = 0
+      -- vim.g.mkdp_browser = '/usr/bin/xdg_open'
+      vim.g.mkdb_echo_preview_url = 1
     end,
     ft = { 'markdown' },
-    mkdp_browser = '/usr/bin/firefox',
-    opts = {},
   },
 }
