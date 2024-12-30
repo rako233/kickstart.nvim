709216f5e1a21150ab406faaf22561b24ce2cdb5
rako233 <rako233@users.noreply.github.com>
2024-12-30 20:50:47 +0100

    mini.statusline was optimized

--- lua/zoe/plugs_auto/mini.lua
+++ lua/zoe/plugs_auto/mini.lua
@@ -17,12 +17,6 @@ return {
       -- - sr)'  - [S]urround [R]eplace [)] [']
       require('mini.surround').setup()
 
-      -- Simple and easy statusline.
-      --  You could remove this setup call if you don't like it,
-      --  and try some other statusline plugin
-      local statusline = require 'mini.statusline'
-      -- set use_icons to true if you have a Nerd Font
-
       local combine_groups = function(groups)
         local parts = vim.tbl_map(function(s)
           if type(s) == 'string' then
@@ -53,43 +47,52 @@ return {
         return table.concat(parts, '')
       end
 
-      -- This function takes a hl class and updates the class with the BG
-      -- of another class. The result is stored in a new group
+      -- This function takes 2 hl classes and creates a new hl class
       --@param hl_fg string : The highlight name of the highlight class
-      --@param hl_bg string : The highlight name of a highlight class
+      --@param hl_bg string : The highlight name of another highlight class
       local make_color = function(hl_fg, hl_bg)
         ---@class vim.api.keyset.highlight
+        ---@diagnostic disable-next-line: assign-type-mismatch
         local fghl = vim.api.nvim_get_hl(0, { name = hl_fg })
         local bghl = vim.api.nvim_get_hl(0, { name = hl_bg })
         fghl.fg = fghl.bg
         fghl.bg = bghl.bg
-        fghl.force = true
         vim.api.nvim_set_hl(0, hl_fg .. '2', fghl)
       end
 
-      statusline.setup {
+      local mode_hl = {}
+      local mode = ''
 
+      local setup_hl = function()
+        _, mode_hl = MiniStatusline.section_mode { trunc_width = 50 }
+        make_color(mode_hl, 'MiniStatuslineFilename')
+        make_color('MiniStatuslineDevinfo', 'MiniStatuslineFilename')
+        make_color('MiniStatuslineFileInfo', 'MiniStatuslineFilename')
+      end
+
+      local modify_mode_group_hl = function()
+        _, mode_hl = MiniStatusline.section_mode { trunc_width = 50 }
+        make_color(mode_hl, 'MiniStatuslineFilename')
+      end
+
+      vim.api.nvim_create_autocmd('VimEnter', { pattern = '*', desc = 'add more hl groups', callback = setup_hl })
+      vim.api.nvim_create_autocmd('ModeChanged', { pattern = '*', desc = 'update hl group for mode', callback = modify_mode_group_hl })
+
+      local statusline = require 'mini.statusline'
+
+      statusline.setup {
         content = {
           -- Content for active window
           active = function()
-            local mode, mode_hl = MiniStatusline.section_mode { trunc_width = 50 }
+            mode, mode_hl = MiniStatusline.section_mode { trunc_width = 50 }
             local git = MiniStatusline.section_git { trunc_width = 40 }
-            local diff = MiniStatusline.section_diff { trunc_width = 75 }
-            local diagnostics = MiniStatusline.section_diagnostics { trunc_width = 75 }
-            local lsp = MiniStatusline.section_lsp { trunc_width = 75 }
-            local filename = MiniStatusline.section_filename { trunc_width = 140 }
-            local fileinfo = MiniStatusline.section_fileinfo { trunc_width = 120 }
-            local location = MiniStatusline.section_location { trunc_width = 75 }
-            local search = MiniStatusline.section_searchcount { trunc_width = 75 }
-
-            -- Usage of `MiniStatusline.combine_groups()` ensures highlighting and
-            -- correct padding with spaces between groups (accounts for 'missing'
-            -- sections, etc.)
-            --
-
-            make_color(mode_hl, 'MiniStatuslineFilename')
-            make_color('MiniStatuslineDevinfo', 'MiniStatuslineFilename')
-            make_color('MiniStatuslineFileInfo', 'MiniStatuslineFilename')
+            local diff = MiniStatusline.section_diff { trunc_width = 50 }
+            local diagnostics = MiniStatusline.section_diagnostics { trunc_width = 80 }
+            local lsp = MiniStatusline.section_lsp { trunc_width = 40 }
+            local filename = MiniStatusline.section_filename { trunc_width = 80 }
+            local fileinfo = MiniStatusline.section_fileinfo { trunc_width = 50 }
+            -- local location = MiniStatusline.section_location { trunc_width = 50 }
+            local search = MiniStatusline.section_searchcount { trunc_width = 50 }
 
             local tab = {
               { hl = mode_hl, strings = { mode } },
@@ -111,24 +114,8 @@ return {
             end
 
             table.insert(tab, { hl = mode_hl .. '2', strings = { '█' } })
-            table.insert(tab, { hl = mode_hl, strings = { search, location } })
+            table.insert(tab, { hl = mode_hl, strings = { search, '%2l:%-2v' } })
             return combine_groups(tab)
-            -- return combine_groups {
-            --   { hl = mode_hl, strings = { mode } },
-            --   { hl = mode_hl .. '2', strings = { '█' } },
-            --   '%<', -- Mark general truncate point
-            --   { hl = 'MiniStatuslineDevinfo2', strings = { '█' } },
-            --   { hl = 'MiniStatuslineDevinfo', strings = { git, diff, diagnostics, lsp } },
-            --   { hl = 'MiniStatuslineDevinfo2', strings = { '█' } },
-            --   '%<', -- Mark general truncate point
-            --   { hl = 'MiniStatuslineFilename', strings = { ' ', filename, ' ' } },
-            --   '%=', -- End left alignment
-            --   { hl = 'MiniStatuslineFileinfo2', strings = { '█' } },
-            --   { hl = 'MiniStatuslineFileinfo', strings = { fileinfo } },
-            --   { hl = 'MiniStatuslineFileinfo2', strings = { '█' } },
-            --   { hl = mode_hl .. '2', strings = { '█' } },
-            --   { hl = mode_hl, strings = { search, location } },
-            -- }
           end,
           -- Content for inactive window(s)
           inactive = nil,
@@ -136,14 +123,6 @@ return {
         use_icons = vim.g.have_nerd_font,
         set_vim_settings = true,
       }
-
-      -- You can configure sections in the statusline by overriding their
-      -- default behavior. For example, here we set the section for
-      -- cursor location to LINE:COLUMN
-      ---@diagnostic disable-next-line: duplicate-set-field
-      statusline.section_location = function()
-        return '%2l:%-2v'
-      end
 
       -- ... and there is more!
       --  Check out: https://github.com/echasnovski/mini.nvim
